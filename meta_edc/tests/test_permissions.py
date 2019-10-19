b62564a415f2d10221772b4c7d869fa56030ade4
erikvw <ew2789@gmail.com>
2019-10-19 01:39:53 -0500

    add tests

---
+++ meta_edc/tests/test_permissions.py
@@ -0,0 +1,8 @@
+# from ambition_permissions.updaters import update_permissions
+# from django.test import tag
+# from edc_permissions.tests.test_group_permissions import TestGroupPermissions
+#
+#
+# class MyTestGroupPermissions(TestGroupPermissions):
+#     def test_permissions(self):
+#         update_permissions()
