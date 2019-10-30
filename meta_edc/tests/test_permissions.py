c6c3b94b722864470445eadedb41392b3f7f55d6
erikvw <ew2789@gmail.com>
2019-10-29 21:32:11 -0500

    remove ref to edc-permissions, meta-permissions

--- meta_edc/tests/test_permissions.py
+++ meta_edc/tests/test_permissions.py
@@ -1,6 +1,6 @@
 # from ambition_permissions.updaters import update_permissions
 # from django.test import tag
-# from edc_permissions.tests.test_group_permissions import TestGroupPermissions
+# from edc_auth.tests.test_group_permissions import TestGroupPermissions
 #
 #
 # class MyTestGroupPermissions(TestGroupPermissions):
