4be192bdc6272bc341195e87b6260824e4c8bd4a
erikvw <ew2789@gmail.com>
2020-01-18 15:42:11 -0600

    update settings, bump requirements

--- meta_edc/tests/test_permissions.py
+++
@@ -1,8 +0,0 @@
-# from ambition_permissions.updaters import update_permissions
-# from django.test import tag
-# from edc_auth.tests.test_group_permissions import TestGroupPermissions
-#
-#
-# class MyTestGroupPermissions(TestGroupPermissions):
-#     def test_permissions(self):
-#         update_permissions()
