0b1c63337a8fb41c854359b91dfd7657bb93bb83
erikvw <ew2789@gmail.com>
2020-04-02 23:41:37 -0500

    flake8

--- meta_edc/settings/debug.py
+++ meta_edc/settings/debug.py
@@ -1,3 +1,5 @@
+import os
+
 from multisite import SiteID
 
 from .defaults import *  # noqa
@@ -11,11 +13,11 @@ ALLOWED_HOSTS = [
     "hindu-mandal.tz.meta.clinicedc.org",
     "localhost",
 ]  # env.list('DJANGO_ALLOWED_HOSTS')
-ETC_DIR = os.path.join(BASE_DIR, "tests", "etc")
-# KEY_PATH = os.path.join(ETC_DIR, "crypto_fields")
-KEY_PATH = os.path.join(BASE_DIR, ".etc", "meta", "crypto_fields")
+ETC_DIR = os.path.join(BASE_DIR, "tests", "etc")  # noqa
+KEY_PATH = os.path.join(ETC_DIR, "crypto_fields")
+# KEY_PATH = os.path.join(BASE_DIR, ".etc", "meta", "crypto_fields")
 
-if os.path.exists(BASE_DIR) and not os.path.exists(KEY_PATH):
+if os.path.exists(BASE_DIR) and not os.path.exists(KEY_PATH):  # noqa
     os.makedirs(KEY_PATH)
     AUTO_CREATE_KEYS = True
 SECURE_SSL_REDIRECT = False
