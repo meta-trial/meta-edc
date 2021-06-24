9e464820b48bfea476fcaf463f7b6151dddad51b
erikvw <ew2789@gmail.com>
2021-06-23 21:42:49 -0500

    move func to respond forms

--- meta_screening/forms/screening_part_three_form.py
+++ meta_screening/forms/screening_part_three_form.py
@@ -1,34 +1,11 @@
 from django import forms
 from edc_form_validators import FormValidatorMixin
-from edc_reportable import convert_units, MILLIMOLES_PER_LITER
 from edc_screening.modelform_mixins import AlreadyConsentedFormMixin
+from respond_forms.utils import validate_glucose_as_millimoles_per_liter
 
 from ..form_validators import ScreeningPartThreeFormValidator
 from ..models import ScreeningPartThree
 from .field_lists import part_three_fields
-
-
-def validate_glucose_as_millimoles_per_liter(
-    cleaned_data=None, min_val=None, max_val=None
-):
-    min_val = min_val or 1.0
-    max_val = max_val or 19.0
-    for fld in ["fasting_glucose", "ogtt_two_hr"]:
-        value = cleaned_data.get(fld)
-        units = cleaned_data.get(f"{fld}_units")
-        if value and units:
-            converted_value = convert_units(
-                value, units_from=units, units_to=MILLIMOLES_PER_LITER
-            )
-            if not (min_val <= converted_value <= max_val):
-                raise forms.ValidationError(
-                    {
-                        fld: (
-                            f"This value is out-of-range. "
-                            f"Got {converted_value} mmol/L"
-                        )
-                    }
-                )
 
 
 class ScreeningPartThreeForm(
