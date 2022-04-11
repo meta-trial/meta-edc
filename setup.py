8454a0711bad9c9f6b05bdc621e90a2a5257dfcf
erikvw <ew2789@gmail.com>
2022-04-11 10:36:09 -0500

    setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name="meta-edc",
     version=VERSION,
-    author=u"Erik van Widenfelt",
+    author="Erik van Widenfelt",
     author_email="ew2789@gmail.com",
     packages=find_packages(),
     include_package_data=True,
