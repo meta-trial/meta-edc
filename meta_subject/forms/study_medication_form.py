cf67d5b1c91e70eb58632a6f57fc960dd431081e
erikvw <ew2789@gmail.com>
2022-04-15 13:24:44 -0500

    use form validator from edc-pharmacy

--- meta_subject/forms/study_medication_form.py
+++ meta_subject/forms/study_medication_form.py
@@ -1,64 +1,31 @@
 from django import forms
-from edc_constants.constants import NO, YES
 from edc_crf.modelform_mixins import CrfModelFormMixin
-from edc_form_validators import INVALID_ERROR, FormValidator
-from edc_pharmacy.models import DosageGuideline, Medication, Rx, RxRefill
+from edc_pharmacy.form_validators import (
+    StudyMedicationFormValidator as BaseStudyMedicationFormValidator,
+)
 from edc_visit_schedule.constants import DAY1
-
-from meta_pharmacy.constants import METFORMIN
 
 from ..models import StudyMedication
 
 
-class StudyMedicationFormValidator(FormValidator):
+class StudyMedicationFormValidator(BaseStudyMedicationFormValidator):
     def clean(self):
         self.validate_half_dose_at_baseline()
-
-        self.required_if(
-            YES, field="order_next", field_required="next_dosage_guideline"
-        )
-        if self.cleaned_data.get("order_next") == NO and self.next_refill:
-            if self.next_refill.active:
-                self.raise_validation_error(
-                    "Invalid. Next refill is already active", INVALID_ERROR
-                )
-        if (
-            self.cleaned_data.get("order_next") == NO
-            and not self.cleaned_data.get("subject_visit").appointment.next
-        ):
-            self.raise_validation_error(
-                "Invalid. This is the last scheduled visit", INVALID_ERROR
-            )
-
-        self.required_if(YES, field="order_next", field_required="next_formulation")
+        super().clean()
 
     def validate_half_dose_at_baseline(self):
+        """Require 1000mg dose at baseline"""
         if (
             self.cleaned_data.get("subject_visit").visit_code == DAY1
             and self.cleaned_data.get("subject_visit").visit_code_sequence == 0
         ):
-            if self.cleaned_data.get("dosage_guideline").dose != 1000:
+            if (
+                self.cleaned_data.get("dosage_guideline")
+                and self.cleaned_data.get("dosage_guideline").dose != 1000
+            ):
                 raise forms.ValidationError(
                     {"dosage_guideline": f"Invalid. Expected 1000mg/day at baseline"}
                 )
-
-    @property
-    def next_refill(self):
-        for obj in RxRefill.objects.filter(
-            rx=self.rx,
-            refill_date__gt=self.cleaned_data.get("refill_date"),
-        ).order_by("refill_date"):
-            return obj
-        return None
-
-    @property
-    def rx(self):
-        return Rx.objects.get(
-            subject_identifier=self.cleaned_data.get(
-                "subject_visit"
-            ).subject_identifier,
-            medication=self.cleaned_data.get("formulation").medication,
-        )
 
 
 class StudyMedicationForm(CrfModelFormMixin, forms.ModelForm):
