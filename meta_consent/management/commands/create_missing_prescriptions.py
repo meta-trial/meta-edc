2a4c33010f9f5ff9e64d5fb97f7c20c1df56f207
erikvw <ew2789@gmail.com>
2022-07-01 10:25:01 +0300

    wrap in exception

--- meta_consent/management/commands/create_missing_prescriptions.py
+++ meta_consent/management/commands/create_missing_prescriptions.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
+from edc_pharmacy.exceptions import PrescriptionAlreadyExists
 from edc_pharmacy.models import Medication, Rx
 from edc_pharmacy.prescribe import create_prescription
+from tqdm import tqdm
 
 from meta_consent.models import SubjectConsent
 from meta_edc.meta_version import get_meta_version
@@ -13,13 +15,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         medication = Medication.objects.get(name=METFORMIN)
         subject_identifiers = Rx.objects.values_list("subject_identifier", flat=True).all()
-        for instance in SubjectConsent.objects.exclude(
+        subject_consents_wo_rx = SubjectConsent.objects.exclude(
             subject_identifier__in=subject_identifiers
-        ):
-            create_prescription(
-                subject_identifier=instance.subject_identifier,
-                report_datetime=instance.consent_datetime,
-                randomizer_name=get_meta_version(),
-                medications=[medication],
-                site=instance.site,
-            )
+        )
+        total = subject_consents_wo_rx.count()
+        for instance in tqdm(subject_consents_wo_rx, total=total):
+            try:
+                create_prescription(
+                    subject_identifier=instance.subject_identifier,
+                    report_datetime=instance.consent_datetime,
+                    randomizer_name=get_meta_version(),
+                    medications=[medication],
+                    site=instance.site,
+                )
+            except PrescriptionAlreadyExists:
+                pass
