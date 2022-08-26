f6b6aa75f4419407b2e5cb1e0217458baff066db
erikvw <ew2789@gmail.com>
2022-08-26 08:01:30 -0500

    change subject listboard to search on identity

--- meta_dashboard/views/subject/listboard/listboard_view.py
+++ meta_dashboard/views/subject/listboard/listboard_view.py
@@ -16,5 +16,5 @@ class SubjectListboardView(BaseSubjectListboardView):
     def extra_search_options(self, search_term) -> List[Q]:
         q_objects = super().extra_search_options(search_term)
         if re.match(r"^[0-9\-]+$", search_term):
-            q_objects.append(Q(hospital_identifier__exact=search_term))
+            q_objects.append(Q(identity__exact=search_term))
         return q_objects
