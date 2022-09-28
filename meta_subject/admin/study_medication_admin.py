2883b009226b4e740f3f6a7e54a646c851965aec
erikvw <ew2789@gmail.com>
2022-09-27 23:00:09 -0500

    create_or_update_rx_refills

--- meta_subject/admin/study_medication_admin.py
+++ meta_subject/admin/study_medication_admin.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib import admin, messages
 from django.contrib.messages import ERROR, SUCCESS
 from django.core.exceptions import ObjectDoesNotExist
@@ -8,6 +10,10 @@ from edc_pharmacy.exceptions import NextStudyMedicationError, RefillCreatorError
 from edc_pharmacy.model_mixins.study_medication_crf_model_mixin import (
     StudyMedicationError,
 )
+from edc_pharmacy.models import DosageGuideline, Formulation, Medication
+from edc_visit_schedule.utils import is_baseline
+
+from meta_pharmacy.constants import METFORMIN
 
 from ..admin_site import meta_subject_admin
 from ..forms import StudyMedicationForm
@@ -65,35 +71,56 @@ class StudyMedicationAdmin(CrfModelAdmin):
     def create_or_update_rx_refills(self, request, queryset):
         updated = 0
         errors = 0
+        medication = Medication.objects.get(name=METFORMIN)
+        formulation = Formulation.objects.get(medication=medication, strength=500)
+        dosage_guideline_baseline = DosageGuideline.objects.get(
+            medication=medication, dose=Decimal("1000.0")
+        )
+        dosage_guideline_followup = DosageGuideline.objects.get(
+            medication=medication, dose=Decimal("2000.0")
+        )
         for obj in StudyMedication.objects.filter(site_id=request.site.id).order_by(
             "modified"
         ):
-            try:
-                obj.save()
-            except NextStudyMedicationError as e:
-                messages.add_message(request, ERROR, f"NextStudyMedicationError: {e}")
-                errors += 1
-            except ObjectDoesNotExist as e:
-                messages.add_message(
-                    request,
-                    ERROR,
-                    (
-                        f"ObjectDoesNotExist: {obj.subject_identifier}, "
-                        f"{obj.related_visit}, {str(e)}"
-                    ),
-                )
-                errors += 1
-            except StudyMedicationError as e:
-                messages.add_message(request, ERROR, f"StudyMedicationError: {e}")
-                errors += 1
-            except RefillCreatorError as e:
-                messages.add_message(request, ERROR, f"RefillCreatorError: {e}")
-                errors += 1
-            except OffstudyError as e:
-                messages.add_message(request, ERROR, f"OffstudyError: {e}")
-                errors += 1
-            else:
-                updated += 1
+            obj.refill_identifier = obj.id
+            if not obj.formulation:
+                obj.formulation = formulation
+            if not obj.dosage_guideline:
+                if is_baseline(obj.related_visit):
+                    obj.dosage_guideline = dosage_guideline_baseline
+                else:
+                    obj.dosage_guideline = dosage_guideline_followup
+            updated, errors = self.rx_refills_save_or_raise(request, obj, updated, errors)
         messages.add_message(
             request, SUCCESS, f"Updated {updated} documents with {errors} errors."
         )
+
+    @staticmethod
+    def rx_refills_save_or_raise(request, obj, updated, errors):
+        try:
+            obj.save()
+        except NextStudyMedicationError as e:
+            messages.add_message(request, ERROR, f"NextStudyMedicationError: {e}")
+            errors += 1
+        except ObjectDoesNotExist as e:
+            messages.add_message(
+                request,
+                ERROR,
+                (
+                    f"ObjectDoesNotExist: {obj.subject_identifier}, "
+                    f"{obj.related_visit}, {str(e)}"
+                ),
+            )
+            errors += 1
+        except StudyMedicationError as e:
+            messages.add_message(request, ERROR, f"StudyMedicationError: {e}")
+            errors += 1
+        except RefillCreatorError as e:
+            messages.add_message(request, ERROR, f"RefillCreatorError: {e}")
+            errors += 1
+        except OffstudyError as e:
+            messages.add_message(request, ERROR, f"OffstudyError: {e}")
+            errors += 1
+        else:
+            updated += 1
+        return updated, errors
