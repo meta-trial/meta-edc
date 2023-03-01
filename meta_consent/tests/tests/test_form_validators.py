d6c21996bdff5a1abba5ea637637015fd1e3b8bb
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-03-01 17:00:02 +0000

    Enable consent tests, and add test confirming consent raises if age provided at screening doesn't match age calculated from consent dob

--- meta_consent/tests/tests/test_form_validators.py
+++ meta_consent/tests/tests/test_form_validators.py
@@ -57,9 +57,9 @@ class TestFormValidators(MetaTestCaseMixin, TestCase):
         validator = SubjectConsentFormValidator(
             cleaned_data=cleaned_data,
         )
-        self.assertRaises(forms.ValidationError, validator.clean)
+        self.assertRaises(forms.ValidationError, validator.validate)
         with self.assertRaises(forms.ValidationError) as cm:
-            validator.clean()
+            validator.validate()
         self.assertIn("consent_datetime", str(cm.exception))
 
     def test_consent_after_eligibility_datetime(self):
@@ -83,3 +83,28 @@ class TestFormValidators(MetaTestCaseMixin, TestCase):
             validator.clean()
         except forms.ValidationError:
             self.fail("ValidationError unexpectedly raised")
+
+    def test_mismatch_between_screening_age_and_calculated_consent_age_raises(self):
+        consent_datetime = self.get_now()
+        data = dict(
+            screening_identifier=self.screening_identifier,
+            gender=FEMALE,
+            dob=self.subject_screening.report_datetime.date()
+            - relativedelta(years=self.subject_screening.age_in_years + 1),
+            consent_datetime=consent_datetime,
+            identity_type=HOSPITAL_NUMBER,
+            identity=self.subject_screening.hospital_identifier,
+            confirm_identity=self.subject_screening.hospital_identifier,
+        )
+        form_validator = SubjectConsentFormValidator(
+            cleaned_data=data,
+        )
+        with self.assertRaises(forms.ValidationError) as cm:
+            form_validator.validate()
+        self.assertIn("dob", cm.exception.error_dict)
+        self.assertIn(
+            "Age mismatch. "
+            "The date of birth entered does not match the age at screening. "
+            "Expected 25. Got 26.",
+            str(cm.exception.error_dict.get("dob")),
+        )
