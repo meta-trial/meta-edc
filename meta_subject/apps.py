0df873b1e599353445ce7c277df6b40e93a77110
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-05-15 17:52:58 +0100

    Update meta_subject AppConfig verbose name to 'META Subject (CRFs)'

--- meta_subject/apps.py
+++ meta_subject/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig as DjangoAppConfig
 
 class AppConfig(DjangoAppConfig):
     name = "meta_subject"
-    verbose_name = "META Subject"
+    verbose_name = "META Subject (CRFs)"
     include_in_administration_section = True
     has_exportable_data = True
     default_auto_field = "django.db.models.BigAutoField"
