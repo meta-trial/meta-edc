77203278bdd81d422fa238b44da3e7d41d4cfb30
erikvw <ew2789@gmail.com>
2024-03-11 22:26:30 -0500

    uat settings

--- meta_edc/settings/uat.py
+++ meta_edc/settings/uat.py
@@ -11,12 +11,12 @@ SITE_ID = SiteID(default=1)
 EDC_SITES_UAT_DOMAIN = True
 EDC_CONSENT_BYPASS_CONSENT_DATETIME_VALIDATION = True
 ALLOWED_HOSTS = [
-    "amana.uat.tz.meta3.clinicedc.org",
-    "hindu-mandal.uat.tz.meta3.clinicedc.org",
-    "mbagala.uat.tz.meta3.clinicedc.org",
-    "mnazi-moja.uat.tz.meta3.clinicedc.org",
-    "mwananyamala.uat.tz.meta3.clinicedc.org",
-    "temeke.uat.tz.meta3.clinicedc.org",
+    "amana.uat.tz.meta4.clinicedc.org",
+    "hindu-mandal.uat.tz.meta4.clinicedc.org",
+    "mbagala.uat.tz.meta4.clinicedc.org",
+    "mnazi-moja.uat.tz.meta4.clinicedc.org",
+    "mwananyamala.uat.tz.meta4.clinicedc.org",
+    "temeke.uat.tz.meta4.clinicedc.org",
 ]
 
 # edc_model_admin
