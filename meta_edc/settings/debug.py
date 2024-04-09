11b932e50a3c533259220be204573efba2e6801f
erikvw <ew2789@gmail.com>
2024-04-08 22:20:38 -0500

    change switch sites and logout to post

--- meta_edc/settings/debug.py
+++ meta_edc/settings/debug.py
@@ -13,7 +13,7 @@ SITE_ID = SiteID(default=10)  # Hindu Mandal
 # SITE_ID = SiteID(default=50)  # Mbagala
 # SITE_ID = SiteID(default=60)  # Mnazi-Moja
 # SITE_ID = SiteID(default=30)  # Temeke
-
+INDEX_PAGE = "http://localhost:8000"
 EDC_SITES_UAT_DOMAIN = False
 DEBUG = True
 ALLOWED_HOSTS = [
