fb3547dd6e7fc9d51582dbc0a8c4708ba2bf2127
erikvw <ew2789@gmail.com>
2024-08-19 20:15:52 -0500

    create celery live/uat/debug

--- meta_edc/__init__.py
+++ meta_edc/__init__.py
@@ -1,7 +1,9 @@
 import os
 from importlib.metadata import PackageNotFoundError, version
 
-from .celery import app as celery_app
+from .celery.debug import app as celery_debug
+from .celery.live import app as celery_live
+from .celery.uat import app as celery_uat
 
 try:
     __version__ = version(os.getcwd().split(os.sep)[-1])
@@ -9,4 +11,4 @@ except PackageNotFoundError:
     __version__ = None
 
 
-__all__ = ["celery_app", "__version__"]
+__all__ = ["celery_live", "celery_uat", "celery_debug", "__version__"]
