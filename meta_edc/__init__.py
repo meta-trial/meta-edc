8cac18f19a6ef9fdf97ba750602655c48d156819
erikvw <ew2789@gmail.com>
2024-08-20 20:05:26 -0500

    change celery setup

--- meta_edc/__init__.py
+++ meta_edc/__init__.py
@@ -1,9 +1,10 @@
 import os
 from importlib.metadata import PackageNotFoundError, version
 
-from .celery.debug import app as celery_debug
-from .celery.live import app as celery_live
-from .celery.uat import app as celery_uat
+from .celery import app as celery_app
+
+# from .celery.live import app as celery_live
+# from .celery.uat import app as celery_uat
 
 try:
     __version__ = version(os.getcwd().split(os.sep)[-1])
@@ -11,4 +12,5 @@ except PackageNotFoundError:
     __version__ = None
 
 
-__all__ = ["celery_live", "celery_uat", "celery_debug", "__version__"]
+# __all__ = ["celery_live", "celery_uat", "celery_debug", "__version__"]
+__all__ = ["__version__", "celery_app"]
