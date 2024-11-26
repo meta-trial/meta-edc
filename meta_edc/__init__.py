158be0b447fe96dc41209dfddc665236d63384bc
erikvw <ew2789@gmail.com>
2024-11-26 13:09:35 -0600

    dont import celery in init

--- meta_edc/__init__.py
+++ meta_edc/__init__.py
@@ -1,7 +1,7 @@
 import os
 from importlib.metadata import PackageNotFoundError, version
 
-from .celery import app as celery_app
+# from .celery import app
 
 try:
     __version__ = version(os.getcwd().split(os.sep)[-1])
@@ -9,6 +9,6 @@ except PackageNotFoundError:
     __version__ = None
 
 
-__all__ = ["celery_app", "__version__"]
+__all__ = ["__version__"]
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meta_edc.settings.debug")
