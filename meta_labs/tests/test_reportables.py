e0feb9827af62a2d4fc6b4de81079a0037aa872a
erikvw <ew2789@gmail.com>
2025-07-09 18:24:33 -0500

    add dummy reportables, bump edc==1.1.5

--- meta_labs/tests/test_reportables.py
+++ meta_labs/tests/test_reportables.py
@@ -1,9 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, tag
+from edc_lab import site_labs
 from edc_reportable.models import GradingData, NormalData, ReferenceRangeCollection
 from edc_reportable.utils import load_reference_ranges
 
 
 class TestReportables(TestCase):
+    @tag("37")
     def test_load_reference_ranges(self):
 
         from meta_labs.reportables import (
@@ -23,15 +25,46 @@ class TestReportables(TestCase):
         )
         self.assertTrue(ReferenceRangeCollection.objects.filter(name=collection_name).exists())
         reference_range_collection = ReferenceRangeCollection.objects.get(name=collection_name)
+
+        for panel_name, requisition_panel in site_labs.lab_profiles.get(
+            "subject_lab_profile"
+        ).panels.items():
+            for utest_id in requisition_panel.utest_ids:
+                try:
+                    utest_id, _ = utest_id
+                except ValueError:
+                    pass
+                if not NormalData.objects.filter(label=utest_id).exists():
+                    print(f"{utest_id} does not exist in NormalData")
+                if not GradingData.objects.filter(label=utest_id).exists():
+                    print(f"{utest_id} does not exist in GradingData")
+
+        for panel_name, requisition_panel in site_labs.lab_profiles.get(
+            "subject_lab_profile"
+        ).panels.items():
+            for utest_id in requisition_panel.utest_ids:
+                try:
+                    utest_id, _ = utest_id
+                except ValueError:
+                    pass
+                self.assertTrue(
+                    NormalData.objects.filter(label=utest_id).exists(),
+                    msg=f"{utest_id} does not exist in NormalData",
+                )
+                self.assertTrue(
+                    GradingData.objects.filter(label=utest_id).exists(),
+                    msg=f"{utest_id} does not exist in GradingData",
+                )
+
         self.assertEqual(
             NormalData.objects.filter(
                 reference_range_collection=reference_range_collection
             ).count(),
-            82,
+            88,
         )
         self.assertEqual(
             GradingData.objects.filter(
                 reference_range_collection=reference_range_collection
             ).count(),
-            174,
+            180,
         )
