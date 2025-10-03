9cf6f8a15dd3fab86c3a23a5a69b784457a94fd3
erikvw <ew2789@gmail.com>
2025-10-03 17:12:37 -0500

    fix issue where unmanaged model gives wrong label lower to notes model

--- src/meta_reports/models/dbviews/glucose_summary/unmanaged_model.py
+++ src/meta_reports/models/dbviews/glucose_summary/unmanaged_model.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_db_views.db_view import DBView
+from edc_constants.constants import NULL_STRING
 from edc_qareports.model_mixins import QaReportModelMixin, qa_reports_permissions
 
 from .view_definition import get_view_definition
@@ -8,17 +9,17 @@ from .view_definition import get_view_definition
 class GlucoseSummary(QaReportModelMixin, DBView):
     fbg_value = models.DecimalField(max_digits=8, decimal_places=2, null=True)
 
-    fbg_units = models.CharField(max_length=15, default="")
+    fbg_units = models.CharField(max_length=15, default=NULL_STRING)
 
     fbg_datetime = models.DateTimeField(null=True)
 
     ogtt_value = models.DecimalField(max_digits=8, decimal_places=2, null=True)
 
-    ogtt_units = models.CharField(max_length=15, default="")
+    ogtt_units = models.CharField(max_length=15, default=NULL_STRING)
 
     ogtt_datetime = models.DateTimeField(null=True)
 
-    fasted = models.CharField(max_length=15, default="")
+    fasted = models.CharField(max_length=15, default=NULL_STRING)
 
     fasting_duration_delta = models.DurationField(
         verbose_name="Fasting duration (hrs)", null=True
@@ -34,7 +35,7 @@ class GlucoseSummary(QaReportModelMixin, DBView):
 
     offstudy_datetime = models.DateTimeField(null=True)
 
-    source = models.CharField(max_length=35, default="")
+    source = models.CharField(max_length=35, default=NULL_STRING)
 
     view_definition = get_view_definition()
 
