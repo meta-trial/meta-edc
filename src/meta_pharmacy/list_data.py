f55732dba9fd2bd425ca01b622d34c8ae6b6a763
erikvw <ew2789@gmail.com>
2026-01-19 07:46:42 -0600

    use constant

--- src/meta_pharmacy/list_data.py
+++ src/meta_pharmacy/list_data.py
@@ -1,8 +1,9 @@
+from edc_pharmacy.constants import CENTRAL_LOCATION
 from edc_sites.site import sites as site_sites
 
 list_data = {
     "edc_pharmacy.location": [
-        ("central", "Central"),
+        (CENTRAL_LOCATION, "Central"),
         *[(ss.name, ss.description) for ss in site_sites.all(aslist=True)],
     ],
 }
